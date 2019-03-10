dc5d6941cd04a44771b8a22c59d42fd2ebf96cb5
brightendev@hotmail.com <brightendev@hotmail.com>
2019-03-10 17:13:06 +0700

    Add project files.

---
+++ JobRecruitmentApi/Function1.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace JobRecruitmentApi
+{
+    public static class Function1
+    {
+
+        [FunctionName("Register")]
+        public static async Task<string> RegisterAccount(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+
+        {
+            log.LogInformation("RegisterAccount Function has been called");
+
+            string accountData = req.Query["account"];
+
+
+            return accountData;
+        }
+    }
+}

2a10d2a57d6ede5491a2f0e107069236a7906058
boy <boychaiwat1997@gmail.com>
2019-03-18 11:17:36 +0700

    สร้าง End point  อ่านข้อมูล จังหวัด,เลือด,ศาสนา,สถานะทางทหาร,สถานะครอบครัว สร้าง Api  อ่านข้อมูล จังหวัด,เลือด,ศาสนา,สถานะทางทหาร,สถานะครอบครัว

--- JobRecruitmentApi/Api/Database.cs
+++ JobRecruitmentApi/Api/Database.cs
@@ -12,6 +12,7 @@ namespace JobRecruitmentApi.Api
             string sqlQuery = $"INSERT INTO [dbo].[Account] ([uid],[email],[role]) VALUES('{uid}','{email}',1);";
             return await AzureResources.SqlDatabase.Insert(sqlQuery);
         }
+
         public static async Task<string> CheckEmail(string email)
         {
             string sqlQuery = $"SELECT * FROM dbo.Account WHERE email='{email}';";
@@ -31,6 +32,30 @@ namespace JobRecruitmentApi.Api
             return await AzureResources.SqlDatabase.Insert(sqlQuery);
 
         }
+
+        public static async Task<string> CheckRole(string uid)
+        {
+            string sqlQuery = $"SELECT dbo.Role.role_name " +
+                $"FROM dbo.Account INNER JOIN dbo.Role ON  dbo.Role.role_id = dbo.Account.role " +
+                $"WHERE dbo.Account.uid = '{uid}'; ";
+            String result = await AzureResources.SqlDatabase.Select(sqlQuery);
+            if (result.Equals("[]")) { return "No Account"; };
+            if (result.Equals("ERROR")) { return "ERROR"; }
+            result = result.Substring(0, result.Length-3);
+            result = result.Substring(15);
+            return result;
+
+        }
+
+        public static async Task<string> GetReligion() => await AzureResources.SqlDatabase.Select($"SELECT * FROM dbo.Religion;");
+
+        public static async Task<string> GetBlood() => await AzureResources.SqlDatabase.Select($"SELECT * FROM dbo.Blood;");
+
+        public static async Task<string> GetRelationship() => await AzureResources.SqlDatabase.Select($"SELECT * FROM dbo.Relationship;");
+
+        public static async Task<string> GetProvince() => await AzureResources.SqlDatabase.Select($"SELECT * FROM dbo.Province;");
+
+        public static async Task<string> GetMilitaryCriterion() => await AzureResources.SqlDatabase.Select($"SELECT * FROM dbo.MilitaryCriterion;");
 
 
     }
